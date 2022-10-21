852da64497aafc276754084b5fd62a45052494c0
arunmhh <arunmhhh@outlook.com>
2022-10-21 22:10:59 +0200

    text file

---
+++ code/get_ghcnd_all_files.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+tar tf data/ghcnd_all.tar.gz > data/ghcnd_all_files.txt
--- driver.bash
+++ driver.bash
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# get all of the daily data from all the weather stations and generate
+#list of stations
 code/get_ghcnd_data.bash ghcnd_all.tar.gz
+code/get_ghcnd_all_files.txt
+
+# get listing of types of data found at each weather stations
+
 code/get_ghcnd_data.bash ghcnd-inventory.txt
+
+#get metadata for each weather station
 code/get_ghcnd_data.bash ghcnd-stations.txt
+
+
+
+
